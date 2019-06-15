67b73e64d431de59c1651d69167694c955253813
Bekele <Bekele@DESKTOP-CNO58LJ>
2019-06-15 12:39:11 -0400

    just testing github code push

--- src/test/java/maven/tested_coded/RunFB.java
+++ src/test/java/maven/tested_coded/RunFB.java
@@ -1,5 +1,4 @@
 package maven.tested_coded;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,6 +19,36 @@ public class RunFB {
 		}
 		
        	
+       	@Test
+       	void run() {
+       		BeginPageFB exc = PageFactory.initElements(driver, BeginPageFB.class);
+       		exc.longin("bekele", "abc123","bk@gmail.com","Aug","23");
+       		
+       		
+       	}
+       	
+} 	
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class RunFB {
+	 WebDriver driver=null;
+	
+		@BeforeClass
+		void rubrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bekele\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");	
+		driver = new ChromeDriver();
+		
+		driver.get("http://facebook.com");
+		}
+		
+       	//this is Github code commit and push test
        	@Test
        	void run() {
        		BeginPageFB exc = PageFactory.initElements(driver, BeginPageFB.class);
